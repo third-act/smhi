6426dce7778053962027c40680587739198added
Alexander Yngling <yngling@thirdact.se>
2022-11-22 10:11:56 +0100

    Air temperature now uses the correct parameter value.

--- src/parameter.rs
+++ src/parameter.rs
@@ -3,5 +3,5 @@ use serde_repr::{Deserialize_repr, Serialize_repr};
 #[derive(Serialize_repr, Deserialize_repr, Debug, PartialEq, Clone, Copy)]
 #[repr(u8)]
 pub enum Parameter {
-    AirTemperature = 26,
+    AirTemperature = 1,
 }
